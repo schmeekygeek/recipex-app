94aa894327cbd76298179de84cffaaa2bb1388cd
schmeekygeek <ss6087982@gmail.com>
2023-03-06 17:33:48 +0530

    animate intro_page and fix navigator not replacing login page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,8 +8,45 @@ import '../extensions.dart';
 import 'login_page.dart';
 import '../providers/theme_model.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> 
+  with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Tween _tween;
+
+  @override
+  void initState() {
+
+    _tween = Tween<double>(
+      begin: 0.9,
+      end: 1
+    );
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 600
+      ),
+    );
+
+    _tween.animate(_controller);
+    _controller.forward();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,151 +56,159 @@ class IntroPage extends StatelessWidget {
           toolbarHeight: 0,
           backgroundColor: Colors.transparent,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            textDirection: TextDirection.ltr,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: OutlinedButton.icon(
-                  style: ButtonStyle(
-                    shape: MaterialStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40),
+        body: SizeTransition(
+          sizeFactor: _controller,
+          axisAlignment: -0.95,
+          axis: Axis.vertical,
+          child: FadeTransition(
+            opacity: _controller,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                textDirection: TextDirection.ltr,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: OutlinedButton.icon(
+                      style: ButtonStyle(
+                        shape: MaterialStatePropertyAll(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                        ),
+                      ),
+                      label: Text(
+                        context.watch<ThemeModel>().isDark ? "Light" : "Dark",
+                      ),
+                      onPressed: () => context.read<ThemeModel>().toggle(),
+                      icon: Icon(
+                        context.watch<ThemeModel>().isDark
+                            ? FontAwesomeIcons.lightbulb
+                            : FontAwesomeIcons.solidMoon,
                       ),
                     ),
                   ),
-                  label: Text(
-                    context.watch<ThemeModel>().isDark ? "Light" : "Dark",
+                  DefaultTextStyle(
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontFamily: "LobsterTwo",
+                          fontSize: 45,
+                        ),
+                    child: AnimatedTextKit(
+                      displayFullTextOnTap: true,
+                      pause: const Duration(milliseconds: 1500),
+                      repeatForever: true,
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          "Quick",
+                        ),
+                        TyperAnimatedText(
+                          "Delicious",
+                        ),
+                        TyperAnimatedText(
+                          "Easy",
+                        ),
+                      ],
+                    ),
                   ),
-                  onPressed: () => context.read<ThemeModel>().toggle(),
-                  icon: Icon(
-                    context.watch<ThemeModel>().isDark
-                        ? FontAwesomeIcons.lightbulb
-                        : FontAwesomeIcons.solidMoon,
+                  Text(
+                    "recipes at your fingertips",
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontFamily: "LobsterTwo",
+                          fontSize: 45,
+                        ),
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Cooking for your loved ones has never been easier.",
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        color: Colors.blueGrey,
+                        fontFamily: "Satoshi",
+                        fontSize: 15,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Center(
+                      child: LottieBuilder.asset(
+                        "assets/girl-cooking.json",
+                        width: 500,
+                        height: 500,
+                      ),
+                    ),
+                  ),
+                  OutlinedButton.icon(
+                    label: const Text("Sign in"),
+                    icon: const Icon(
+                      FontAwesomeIcons.arrowRightToBracket,
+                    ),
+                    onPressed: () => context.push(const LoginPage()),
+                    style: ButtonStyle(
+                      shape: MaterialStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                      iconSize: const MaterialStatePropertyAll(17),
+                      fixedSize: const MaterialStatePropertyAll(
+                        Size(double.maxFinite, 45),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  FilledButton.icon(
+                    label: const Text("Register"),
+                    icon: const Icon(
+                      FontAwesomeIcons.userPlus,
+                    ),
+                    onPressed: () => {},
+                    style: ButtonStyle(
+                      shape: MaterialStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                      iconSize: const MaterialStatePropertyAll(17),
+                      fixedSize: const MaterialStatePropertyAll(
+                        Size(double.maxFinite, 45),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  FilledButton.icon(
+                    label: const Text("Continue as guest"),
+                    icon: const Icon(
+                      FontAwesomeIcons.userSlash,
+                    ),
+                    onPressed: () => {},
+                    style: ButtonStyle(
+                      foregroundColor: const MaterialStatePropertyAll(
+                        White.text,
+                      ),
+                      shape: MaterialStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                      iconSize: const MaterialStatePropertyAll(17),
+                      fixedSize: const MaterialStatePropertyAll(
+                        Size(
+                          double.maxFinite,
+                          45,
+                        ),
+                      ),
+                      backgroundColor: const MaterialStatePropertyAll(
+                        Colors.lime,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              DefaultTextStyle(
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      fontFamily: "LobsterTwo",
-                      fontSize: 45,
-                    ),
-                child: AnimatedTextKit(
-                  displayFullTextOnTap: true,
-                  pause: const Duration(milliseconds: 1500),
-                  repeatForever: true,
-                  animatedTexts: [
-                    TyperAnimatedText(
-                      "Quick",
-                    ),
-                    TyperAnimatedText(
-                      "Delicious",
-                    ),
-                    TyperAnimatedText(
-                      "Easy",
-                    ),
-                  ],
-                ),
-              ),
-              Text(
-                "recipes at your fingertips",
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      fontFamily: "LobsterTwo",
-                      fontSize: 45,
-                    ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "Cooking for your loved ones has never been easier.",
-                style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                    color: Colors.blueGrey,
-                    fontFamily: "Satoshi",
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600),
-              ),
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: LottieBuilder.asset(
-                    "assets/girl-cooking.json",
-                    width: 500,
-                    height: 500,
-                  ),
-                ),
-              ),
-              OutlinedButton.icon(
-                label: const Text("Sign in"),
-                icon: const Icon(
-                  FontAwesomeIcons.arrowRightToBracket,
-                ),
-                onPressed: () => context.push(const LoginPage()),
-                style: ButtonStyle(
-                  shape: MaterialStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                  ),
-                  iconSize: const MaterialStatePropertyAll(17),
-                  fixedSize: const MaterialStatePropertyAll(
-                    Size(double.maxFinite, 45),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              FilledButton.icon(
-                label: const Text("Register"),
-                icon: const Icon(
-                  FontAwesomeIcons.userPlus,
-                ),
-                onPressed: () => {},
-                style: ButtonStyle(
-                  shape: MaterialStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                  ),
-                  iconSize: const MaterialStatePropertyAll(17),
-                  fixedSize: const MaterialStatePropertyAll(
-                    Size(double.maxFinite, 45),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              FilledButton.icon(
-                label: const Text("Continue as guest"),
-                icon: const Icon(
-                  FontAwesomeIcons.userSlash,
-                ),
-                onPressed: () => {},
-                style: ButtonStyle(
-                  foregroundColor: const MaterialStatePropertyAll(
-                    White.text,
-                  ),
-                  shape: MaterialStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                  ),
-                  iconSize: const MaterialStatePropertyAll(17),
-                  fixedSize: const MaterialStatePropertyAll(
-                    Size(
-                      double.maxFinite,
-                      45,
-                    ),
-                  ),
-                  backgroundColor: const MaterialStatePropertyAll(
-                    Colors.lime,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,10 +6,14 @@ import '../pages/home.dart';
 import '../shared/password_text_form_field.dart';
 import '../shared/username_login_text_form_field.dart';
 
-class LoginPage extends StatelessWidget {
-
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -27,9 +31,7 @@ class LoginPage extends StatelessWidget {
               Align(
                 alignment: Alignment.centerLeft,
                 child: IconButton(
-                  icon: const Icon(
-                    Icons.arrow_back_rounded
-                  ),
+                  icon: const Icon(Icons.arrow_back_rounded),
                   onPressed: () => context.pop(),
                 ),
               ),
@@ -95,15 +97,20 @@ class LoginPage extends StatelessWidget {
               const SizedBox(height: 20),
               FilledButton(
                 onPressed: () {
+                  context.pop();
                   context.pushReplacement(const Home());
                 },
                 style: ButtonStyle(
                   shape: MaterialStatePropertyAll(
                     RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4)),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
                   ),
                   fixedSize: const MaterialStatePropertyAll(
-                    Size(double.maxFinite, 40),
+                    Size(
+                      double.maxFinite,
+                      40,
+                    ),
                   ),
                 ),
                 child: const Text(
@@ -149,132 +156,3 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
-
-// return Scaffold(
-   //   body: Padding(
-   //     padding: const EdgeInsets.all(12.0),
-   //     child: SingleChildScrollView(
-   //       child: Column(
-   //         crossAxisAlignment: CrossAxisAlignment.center,
-   //         mainAxisAlignment: MainAxisAlignment.end,
-   //         children: [
-   //           Align(
-   //             alignment: Alignment.centerLeft,
-   //             child: IconButton(
-   //               icon: const Icon(
-   //                 FontAwesomeIcons.arrowLeftLong,
-   //               ),
-   //               onPressed: () => context.pop(),
-   //             ),
-   //           ),
-   //           LottieBuilder.asset(
-   //             "assets/girl-cooking2.json",
-   //             frameRate: FrameRate.max,
-   //             width: 230,
-   //           ),
-   //           const Text(
-   //             "Glad to have\nyou back!",
-   //             textAlign: TextAlign.center,
-   //             style: TextStyle(
-   //               fontSize: 40,
-   //               fontWeight: FontWeight.w100,
-   //               fontFamily: "LobsterTwo",
-   //             ),
-   //           ),
-   //           const SizedBox(
-   //             height: 15,
-   //           ),
-   //           const Text(
-   //             "Sign in",
-   //             textAlign: TextAlign.center,
-   //             style: TextStyle(
-   //               fontFamily: "Staatliches",
-   //               fontSize: 20,
-   //               letterSpacing: 3,
-   //               color: Colors.blueGrey,
-   //             ),
-   //           ),
-   //           const SizedBox(height: 16),
-   //           const UsernameTextFormField(),
-   //           const SizedBox(height: 10),
-   //           const PasswordTextFormField(),
-   //           const SizedBox(height: 6),
-   //           GestureDetector(
-   //             onTap: () => ScaffoldMessenger.of(context).showSnackBar(
-   //               const SnackBar(
-   //                 content: Text("Coming soon!"),
-   //                 shape: RoundedRectangleBorder(
-   //                   borderRadius: BorderRadius.only(
-   //                     topLeft: Radius.circular(5),
-   //                     topRight: Radius.circular(5),
-   //                   ),
-   //                 ),
-   //               ),
-   //             ),
-   //             child: const Align(
-   //               alignment: Alignment.centerRight,
-   //               child: Text(
-   //                 "Forgot password?",
-   //                 style: TextStyle(
-   //                   fontSize: 13,
-   //                   fontFamily: "Satoshi",
-   //                   color: Colors.blueGrey,
-   //                 ),
-   //               ),
-   //             ),
-   //           ),
-   //           const SizedBox(height: 20),
-   //           FilledButton(
-   //             onPressed: () async {
-   //               context.push(const Home());
-   //             },
-   //             style: ButtonStyle(
-   //               shape: MaterialStatePropertyAll(
-   //                 RoundedRectangleBorder(
-   //                     borderRadius: BorderRadius.circular(4)),
-   //               ),
-   //               fixedSize: const MaterialStatePropertyAll(
-   //                 Size(double.maxFinite, 40),
-   //               ),
-   //             ),
-   //             child: const Text(
-   //               "LOGIN",
-   //             ),
-   //           ),
-   //           const SizedBox(height: 20),
-   //           Row(
-   //             mainAxisAlignment: MainAxisAlignment.center,
-   //             children: [
-   //               Text(
-   //                 "No account? ",
-   //                 style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-   //                       fontSize: 13,
-   //                     ),
-   //               ),
-   //               GestureDetector(
-   //                 onTap: () => ScaffoldMessenger.of(context).showSnackBar(
-   //                   const SnackBar(
-   //                     content: Text("Coming soon!"),
-   //                     shape: RoundedRectangleBorder(
-   //                       borderRadius: BorderRadius.only(
-   //                         topLeft: Radius.circular(5),
-   //                         topRight: Radius.circular(5),
-   //                       ),
-   //                     ),
-   //                   ),
-   //                 ),
-   //                 child: Text(
-   //                   "Create one!",
-   //                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-   //                         fontSize: 13,
-   //                         color: Colors.lightBlue,
-   //                       ),
-   //                 ),
-   //               ),
-   //             ],
-   //           ),
-   //         ],
-   //       ),
-   //     ),
-   //   ),
-   // );
